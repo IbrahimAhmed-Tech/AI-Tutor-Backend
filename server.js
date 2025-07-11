a280b4ad119d5f2e9d2d664559768a7e534a9a02
SheikhIbrahimAhmed <193325@pakaims.edu.pk>
2025-07-12 01:04:01 +0500

    server.js updated and vercel.json added for deployment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require('cors');
 const path = require("path"); 
+const serverless = require("serverless-http"); 
+
 require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
 const supabase = require('./supabaseClient');
 const authRouter = require('./src/routes/authRoutes')
 const aiConversationRoutes = require('./src/routes/aiConversationRoutes');
@@ -19,4 +20,5 @@ app.use('/api/auth', authRouter);
 app.use('/api/ai-conversation', aiConversationRoutes);
 app.use('/public', express.static(path.join(__dirname, 'src', 'public')));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+module.exports.handler = serverless(app);
