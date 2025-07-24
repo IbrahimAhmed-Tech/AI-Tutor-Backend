5027c179f6fb381351e846e453329e7a0ef5f3a2
SheikhIbrahimAhmed <193325@pakaims.edu.pk>
2025-07-24 15:25:06 +0500

    PredefinedResponsesFile updated

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-const app = require('../server'); // assuming app is defined elsewhere
+//  api/index.js is created solely for the purpose of 
+// deploying the app to vercel. This will be the entry
+//  point defined in vercel.json and this is where
+//  I'm making it serverless app which vercel supports.
+
+const app = require('../server'); 
 const serverless = require('serverless-http');
 
 module.exports = serverless(app);
